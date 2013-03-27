8f9393479da97ce19d17fa27b2f0b91381af19b3
Julien Deniau <julien.deniau@gmail.com>
2013-03-27 17:57:57 +0100

    Use $request->server information for more precision

--- StatsCollector/TimeStatsCollector.php
+++ StatsCollector/TimeStatsCollector.php
@@ -9,27 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 class TimeStatsCollector extends StatsCollector
 {
     /**
-     * startTime
-     * 
-     * @var float
-     * @access private
-     */
-    private $startTime;
-
-    /**
-     * __construct
-     *
-     * @access public
-     * @return void
-     */
-    public function __construct($stat_key = __CLASS__, StatsdDataFactoryInterface $stats_data_factory = null, $only_on_master_response = false)
-    {
-        parent::__construct($stat_key, $stats_data_factory, $only_on_master_response);
-
-        $this->startTime = microtime(true);
-    }
-
-    /**
      * Collects data for the given Response.
      *
      * @param Request    $request   A Request instance
@@ -40,8 +19,11 @@ class TimeStatsCollector extends StatsCollector
      */
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-        $time = microtime(true) - $this->startTime;
+        $startTime = $request->server->get('REQUEST_TIME_FLOAT', $request->server->get('REQUEST_TIME'));
+
+        $time = microtime(true) - $startTime;
         $time = round($time * 1000);
+
         $statData = $this->getStatsdDataFactory()->timing($this->getStatsDataKey(), $time);
         $this->addStatsData($statData);
 
