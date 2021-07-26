397f547872357e90746da2456723cdc32cb62e84
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2021-07-26 17:08:06 +0200

    Symfony5 support (#80)

--- Tests/StatsCollector/ExceptionStatsCollectorTest.php
+++ Tests/StatsCollector/ExceptionStatsCollectorTest.php
@@ -4,6 +4,7 @@ namespace Liuggio\StatsDClientBundle\Tests\StatsCollector;
 
 use Liuggio\StatsDClientBundle\StatsCollector\ExceptionStatsCollector;
 use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\ErrorHandler\Exception\FlattenException as ErrorFlattenException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,7 +14,12 @@ class ExceptionStatsCollectorTest extends StatsCollectorBase
     {
         $e = new \Exception('foo', 500);
         $c = new ExceptionStatsCollector('prefix', $this->mockStatsDFactory('prefix.500'));
-        $flattened = FlattenException::create($e);
+        // Symfony >= 4.4 behaviour
+        if(class_exists(ErrorFlattenException::class)) {
+            $flattened = ErrorFlattenException::create($e);
+        } else {
+            $flattened = FlattenException::create($e);
+        }
         $trace = $flattened->getTrace();
 
         $c->collect(new Request(), new Response(), $e);
